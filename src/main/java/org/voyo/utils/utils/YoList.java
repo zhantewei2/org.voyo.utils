31d48b709cc4452d5a24486fcd080cce6e5c15c3
zhan <zhantewei@gmail.com>
2022-06-30 15:41:30 +0800

    add findFirst

---
+++ src/main/java/org/voyo/utils/utils/YoList.java
@@ -0,0 +1,61 @@
+package org.voyo.utils.utils;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class YoList {
+    public static String longToString(List<Long> ids,String sep){
+        StringBuilder stringBuilder=new StringBuilder();
+        ids.forEach(id->{
+            stringBuilder.append(String.valueOf(id)).append(sep);
+        });
+        String content= stringBuilder.toString();
+        return content.substring(0,content.length()-1);
+    }
+    public static List<Long> longFromString(String content,String sep){
+        return Arrays
+                .stream(content.split(sep))
+                .filter(i->i!=null&&!"".equals(i))
+                .map(Long::valueOf)
+                .collect(Collectors.toList());
+    }
+    public static List<Long> longFromString(String content){
+        return longFromString(content,",");
+    }
+
+    public static String longToStringDefault(List<Long> ids){
+        return longToString(ids,",");
+    }
+
+    public static String toString(List<String> ids,String sep){
+        return String.join(sep,ids);
+    }
+    public static List<String> fromString(String content,String sep){
+        return Arrays
+                .stream(content.split(sep))
+                .filter(i->i!=null&!"".equals(i))
+                .collect(Collectors.toList());
+    }
+    public static List<String> fromString(String content){
+        return fromString(content,",");
+    }
+
+    public static String toStringDefault(List<String> ids){
+        return toString(ids,",");
+    }
+
+    public static <T> T findFirst(List<T> list,Function<T,Boolean> run){
+        Iterator<T> iterator= list.stream().iterator();
+        while(iterator.hasNext()){
+            T i= iterator.next();
+            if(run.apply(i)) {
+                return i;
+            }
+        }
+        return null;
+    }
+}
--- src/main/java/org/voyo/utils/utils/YoMath.java
+++ src/main/java/org/voyo/utils/utils/YoMath.java
@@ -33,4 +33,23 @@ public class YoMath {
   public static int randomInt(int start, int end) {
     return Double.valueOf(Math.floor(Math.random() * (double)(end - start))).intValue() + start;
   }
+
+  final static public String miniSource="qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM0123456789*~";
+  final static public Long miniSourceLen=64L;
+
+  public static String miniLong(Long source){
+    Long y;
+    StringBuilder stringBuilder=new StringBuilder();
+    do{
+      y=source% miniSourceLen;
+      source=source/miniSourceLen;
+      stringBuilder.append(miniSource.charAt(y.intValue()));
+    }while(source>= miniSourceLen);
+    return stringBuilder.toString();
+  }
+
+//  public static void main(String[] args){
+//    String r=miniLong(1468883403674775555L);
+//    System.out.println(r);
+//  }
 }
