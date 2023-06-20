3389119ec259fabc76aeb1f0adc2cc95f5af555f
zhantewei2 <zhantewei@gmail.com>
2023-06-20 12:50:51 +0800

    add reqBadEnum

--- src/main/java/org/voyo/utils/HttpException/ReqBadEnum.java
+++ src/main/java/org/voyo/utils/HttpException/ReqBadEnum.java
@@ -8,16 +8,21 @@ public enum ReqBadEnum {
   ExpiredLogin(3,HttpStatus.UNAUTHORIZED),
   NotAuth(4,HttpStatus.UNAUTHORIZED),
   PaymentRequired(5,HttpStatus.PAYMENT_REQUIRED),
+  Timeout(10,HttpStatus.REQUEST_TIMEOUT),
   PaymentError(51,HttpStatus.PAYMENT_REQUIRED),
   NotAccept(61,HttpStatus.NOT_ACCEPTABLE),
   SQLError(110,HttpStatus.BAD_REQUEST),
   FindEmpty(101,HttpStatus.BAD_REQUEST),
   FindUnAuth(102,HttpStatus.BAD_REQUEST),
+  FindOneDup(103,HttpStatus.BAD_REQUEST),
   InsertDup(201,HttpStatus.BAD_REQUEST),
   InsertFail(202,HttpStatus.EXPECTATION_FAILED),
   UpdateNotFound(301,HttpStatus.BAD_REQUEST),
   UpdateAlreadyExist(302,HttpStatus.BAD_REQUEST),
-  BadFeign(407,HttpStatus.BAD_REQUEST);
+  UpdateError(310,HttpStatus.BAD_REQUEST),
+  BadFeign(407,HttpStatus.BAD_REQUEST),
+  VisitBusy(501,HttpStatus.BAD_REQUEST),
+  VisitLimitation(502,HttpStatus.BAD_REQUEST);
 
   private int code;
   private HttpStatus httpStatus;
