115542486c27f3e7af8ea4bb155070db0768397d
zhantewei2 <zhantewei@gmail.com>
2023-07-26 11:33:49 +0800

    s

--- src/main/java/org/voyo/utils/jsonValidator/ListNotEmpty.java
+++ src/main/java/org/voyo/utils/jsonValidator/ListNotEmpty.java
@@ -11,7 +11,7 @@ import java.lang.annotation.*;
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 public @interface ListNotEmpty {
-    String message() default "列表不可为空"   ;
+    String message() default "列表不可为空";
     Class<?>[] groups() default {};
     int[] values() default {};
     Class<? extends Payload>[] payload() default {};
