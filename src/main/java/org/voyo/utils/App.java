20fa9ac9e3d81a0c4523da8071f3106346c5cfbb
zhantewei2 <zhantewei@gmail.com>
2023-09-04 16:49:30 +0800

    debug yoUrl

--- src/main/java/org/voyo/utils/App.java
+++ src/main/java/org/voyo/utils/App.java
@@ -1,7 +1,21 @@
 package org.voyo.utils;
 
+import org.voyo.utils.utils.YoDate;
+import org.voyo.utils.utils.url.YoUrl;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.*;
+
 public class App {
     public static void main(String[] args){
+        String url="https://blog.csdn.net/weixin_42501688/article/details/114617205?name=1&&age=2#wechat";
+        String url2=YoUrl.additionUrl(url,new HashMap<String,Object>(){{
+            put("ztwx","http://www.163.com");
+        }});
+        System.out.println(url2);
+        System.out.println(YoUrl.parse(url2).getQuery());
 
     }
 }
