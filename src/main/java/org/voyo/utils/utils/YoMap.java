6474672840e3ac2cd19a4325dfc9756d04036b5e
zhantewei2 <zhantewei@gmail.com>
2023-09-04 17:40:13 +0800

    MapNode

--- src/main/java/org/voyo/utils/utils/YoMap.java
+++ src/main/java/org/voyo/utils/utils/YoMap.java
@@ -4,6 +4,17 @@ package org.voyo.utils.utils;
 import java.util.*;
 
 public class YoMap {
+
+  public static class MapNode<T,S>{
+      private HashMap<T,S> map=new HashMap<>();
+      public MapNode<T,S> put(T k,S v){
+        map.put(k,v);
+        return this;
+      }
+      public HashMap<T,S> get(){
+        return this.map;
+      }
+  }
   public static String getStr(Map<String, Object> m, String key) {
     Object val = m.get(key);
     return val != null ? String.valueOf(val) : null;
@@ -19,6 +30,9 @@ public class YoMap {
     return val != null ? Long.valueOf(String.valueOf(val)) : null;
   }
 
-  public static void main(String[] args) {
+
+  public static <S> MapNode<String,S> buildMap(){
+    return new MapNode<String,S>();
   }
+  public static void main(String[] args) {}
 }
